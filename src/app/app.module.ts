984ff3fc1be6c2a0d3c893b3c70506102fffd02f
Gihan Madushanka <gihan.u@softcodeit.com>
2018-01-19 14:45:31 +0530

    working on the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import {FormsModule} from '@angular/forms'
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component'
+import { WelcomeComponent } from './home/welcome.component';
+import {RouterModule} from '@angular/router';
+import { ProductGuardService } from './products/product-guard.service'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    WelcomeComponent,
+    ProductDetailComponent
+
   ],
   imports: [
-    BrowserModule
-    
-  ],
-  providers: [],
+    BrowserModule,
+    FormsModule,
+    HttpClientModule ,
+    RouterModule.forRoot([
+      {path : 'products', component : ProductListComponent},
+      {path : 'products/:id', canActivate : [ProductGuardService], component : ProductDetailComponent},
+      {path : 'welcome', component : WelcomeComponent},
+      {path: '', redirectTo : 'welcome', pathMatch : 'full'},
+      {path:'**', redirectTo : 'welcome', pathMatch : 'full'}
+
+
+
+
+  ])
+  ], 
+  providers: [ProductGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
